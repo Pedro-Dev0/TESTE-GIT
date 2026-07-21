948e7bfa753e01ebccf62a1f7e023243f44c38de
Pedro-Dev0 <henripedro652@gmail.com>
2026-07-21 16:01:22 -0300

    CRUD arrumando para PEP8]

--- Lessons/install.py
+++ Lessons/install.py
@@ -1,2 +1,4 @@
 #echo "cd ~/Documents/Github" >> ~/.bash_profile para ficar como pasta principal do bash
 import sqlite3
+#PS C:\Users\suporte\Documents\GitHub\TESTE-GIT\BACK-END\src\model> poetry run flake8 .
+#PS C:\Users\suporte\Documents\GitHub\TESTE-GIT\BACK-END\src\model> poetry run isort . 
