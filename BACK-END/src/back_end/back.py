b2158874fc72cd67a728c1a6ec1d19992a8f8b85
Pedro-Dev0 <henripedro652@gmail.com>
2026-07-21 12:35:22 -0300

    SQL PURO

--- BACK-END/src/back_end/back.py
+++ BACK-END/src/back_end/back.py
@@ -8,22 +8,32 @@ with sqlite3.connect(ROOT_PATH / 'meu_banco.db') as con: # con é conexão
     cur = con.cursor() # cur é cursor só que abreviado
 
 def criar_tabela(cur, con):
-    cur.execute('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+    cur.execute("CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))")
     con.commit()
 
 def inserir_registros(con, cur, nome, email):
     data = (nome, email)
-    cur.execute('INSERT INTO clientes (nome, email) VALUES (?,?)' , data)
+    cur.execute("INSERT INTO clientes (nome, email) VALUES (?,?)" , data)
     con.commit()
 
 def atualizar_registro(con, cur, nome, email, id):
     data = (nome, email, id)
-    cur.execute('UPDATE clientes SET nome=?, email=? WHERE id=?;', data)
+    cur.execute("UPDATE clientes SET nome=?, email=? WHERE id=?;", data)
     con.commit()
 
 def deletar_registro(con, cur, id):
     data = (id,)
-    cur.execute('DELETE FROM clientes WHERE id=?;', data)
+    cur.execute("DELETE FROM clientes WHERE id=?;", data)
     con.commit()
 
-inserir_registros(con, cur, 'PEDROA', 'pedroaaaah@outlook.com' )
+def inserir_muitos(con, cur, dados):
+    cur.executemany("INSERT INTO clientes (nome, email), VALUES (?,?)", dados)
+    con.commit()
+
+dados = [
+    ("Guilherme", "guilherme@gmail.com"),
+    ("Larissa", "larissa@gmail.com"),
+    ("Vitoria", "vitoria@gmail.com"),
+]
+
+inserir_muitos(con, cur, dados)
