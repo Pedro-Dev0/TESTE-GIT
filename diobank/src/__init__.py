c6cab8b38b282f56244bea20d3b21eb467a5227c
Pedro-Dev0 <henripedro652@gmail.com>
2026-08-13 15:29:20 -0300

    refazendo e reaprendendo

--- diobank/src/__init__.py
+++ diobank/src/__init__.py
@@ -5,8 +5,8 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)  # avisa que tem coisas sensiveis para não subir no git ou controle de versão
 
     app.config.from_mapping(
-        SECRET_KEY='dev'
-        DATABASE='diobank.sqlite'
+        SECRET_KEY='dev',
+        DATABASE='diobank.sqlite',
     )
 
     if test_config is None:
