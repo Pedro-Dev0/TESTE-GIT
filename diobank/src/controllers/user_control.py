85553f734a5244a4e39d73d8be106a9fbde52fc0
Pedro-Dev0 <henripedro652@gmail.com>
2026-08-18 10:41:56 -0300

    fazendo um post

--- diobank/src/controllers/user_control.py
+++ diobank/src/controllers/user_control.py
@@ -2,18 +2,18 @@ from flask import Blueprint, request
 from src import User, db
 from http import HTTPStatus
 
-app = Blueprint('user', __name__, url_prefix='/users') # sempre plural o url padrão RESTful
+app = Blueprint("user", __name__, url_prefix="/users") # sempre plural o url padrão RESTful
 
-def create_user():
+def _create_user():
     data = request.json
-    user = user(username=data['username'])
+    user = User(username=data["username"])
     db.session.add(user)
     db.session.commit()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods= ["GET", 'POST'])
 def handle_user():
-    if request.method == 'POST':
-        create_user()
+    if request.method == "POST":
+        _create_user()
         return {"message": "User created!"}, HTTPStatus.CREATED
     else:
         return {"users": []}
