ca5c9c7ccae975f6b1add23564d486b66899f7b1
Pedro-Dev0 <henripedro652@gmail.com>
2026-08-31 17:50:47 -0300

    aprendendo sobre teste e pytest

--- diobank/tests/test_utils.py
+++ diobank/tests/test_utils.py
@@ -1,6 +1,22 @@
+import pytest
 from src.utils import eleva_quadrado
 
-def test_eleva_quadrado_sucesso():
+@pytest.mark.parametrize("test_input,expected", [(2, 4), (3, 9), (5, 25)])
+def test_eleva_quadrado_sucesso(test_input, expected):
+    resultado = eleva_quadrado(test_input)
+    assert resultado == expected
+
+def test_eleva_quadrado_sucess():
     resultado = eleva_quadrado(2)
-    assert resultado == 4
+    resultado == 4
+
+@pytest.mark.parametrize("test_input,exc_class,msg", [
+    ("a", TypeError, "unsupported operand type(s) for ** or pow(): 'str' and 'int'"),
+    (None, TypeError, "unsupported operand type(s) for ** or pow(): 'NoneType' and 'int'")
+])
+def test_eleva_quadrado_falha(test_input, exc_class, msg):
+    with pytest.raises(exc_class) as exc:
+        eleva_quadrado(test_input)
+    assert str(exc.value) == msg
+
 
