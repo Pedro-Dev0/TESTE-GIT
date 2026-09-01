d6292939417707a9d3a9eb93d8f00745aa76cd0a
Pedro-Dev0 <henripedro652@gmail.com>
2026-09-01 15:00:07 -0300

    aprendendo a mockar em testes

--- diobank/tests/test_utils.py
+++ diobank/tests/test_utils.py
@@ -1,5 +1,9 @@
+from unittest.mock import Mock, patch
 import pytest
-from src.utils import eleva_quadrado
+from src.utils import eleva_quadrado, requires_role
+from http import HTTPStatus
+
+#Mock(spec=)
 
 @pytest.mark.parametrize("test_input,expected", [(2, 4), (3, 9), (5, 25)])
 def test_eleva_quadrado_sucesso(test_input, expected):
@@ -8,7 +12,7 @@ def test_eleva_quadrado_sucesso(test_input, expected):
 
 def test_eleva_quadrado_sucess():
     resultado = eleva_quadrado(2)
-    resultado == 4
+    assert resultado == 4
 
 @pytest.mark.parametrize("test_input,exc_class,msg", [
     ("a", TypeError, "unsupported operand type(s) for ** or pow(): 'str' and 'int'"),
@@ -19,4 +23,38 @@ def test_eleva_quadrado_falha(test_input, exc_class, msg):
         eleva_quadrado(test_input)
     assert str(exc.value) == msg
 
+def test_requires_role_success():
+    mock_user = Mock()
+    mock_user.role.name = "admin"
 
+    mock_get_jwt_identity = patch('src.utils.get_jwt_identity')
+    mock_db_get_or_404 = patch('src.utils.db.get_or_404', return_value=mock_user)
+    mock_get_jwt_identity.start()
+    mock_db_get_or_404.start()
+
+    decorated_function = requires_role("admin")(lambda: "success")
+
+    result = decorated_function()
+
+    assert result == "success"
+
+    mock_get_jwt_identity.stop()
+    mock_db_get_or_404.stop()
+
+def test_requires_role_fail():
+    mock_user = Mock()
+    mock_user.role.name = "normal"
+
+    mock_get_jwt_identity = patch('src.utils.get_jwt_identity')
+    mock_db_get_or_404 = patch('src.utils.db.get_or_404', return_value=mock_user)
+    mock_get_jwt_identity.start()
+    mock_db_get_or_404.start()
+
+    decorated_function = requires_role("admin")(lambda: "success")
+
+    result = decorated_function()
+
+    assert result == ({"msg": "User dont have access"}, HTTPStatus.FORBIDDEN)
+
+    mock_get_jwt_identity.stop()
+    mock_db_get_or_404.stop()
