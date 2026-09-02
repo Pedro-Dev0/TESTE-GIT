947c6625bb819f82488db2f3db388a1a3b61a4cf
Pedro-Dev0 <henripedro652@gmail.com>
2026-09-02 13:08:39 -0300

    testes com mocker

--- diobank/tests/test_utils.py
+++ diobank/tests/test_utils.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 import pytest
 from src.utils import eleva_quadrado, requires_role
 from http import HTTPStatus
@@ -23,38 +23,33 @@ def test_eleva_quadrado_falha(test_input, exc_class, msg):
         eleva_quadrado(test_input)
     assert str(exc.value) == msg
 
-def test_requires_role_success():
-    mock_user = Mock()
+
+
+
+
+def test_requires_role_success(mocker):
+    #given (forneço)
+    mock_user = mocker.Mock()
     mock_user.role.name = "admin"
 
-    mock_get_jwt_identity = patch('src.utils.get_jwt_identity')
-    mock_db_get_or_404 = patch('src.utils.db.get_or_404', return_value=mock_user)
-    mock_get_jwt_identity.start()
-    mock_db_get_or_404.start()
-
+    mocker.patch('src.utils.get_jwt_identity')
+    mocker.patch('src.utils.db.get_or_404', return_value=mock_user)
     decorated_function = requires_role("admin")(lambda: "success")
 
+    #when(executo)
     result = decorated_function()
 
+    #then(verifico)
     assert result == "success"
 
-    mock_get_jwt_identity.stop()
-    mock_db_get_or_404.stop()
-
-def test_requires_role_fail():
-    mock_user = Mock()
+def test_requires_role_fail(mocker):
+    mock_user = mocker.Mock()
     mock_user.role.name = "normal"
-
-    mock_get_jwt_identity = patch('src.utils.get_jwt_identity')
-    mock_db_get_or_404 = patch('src.utils.db.get_or_404', return_value=mock_user)
-    mock_get_jwt_identity.start()
-    mock_db_get_or_404.start()
-
+    
+    mocker.patch('src.utils.get_jwt_identity')
+    mocker.patch('src.utils.db.get_or_404', return_value=mock_user)
     decorated_function = requires_role("admin")(lambda: "success")
 
     result = decorated_function()
 
     assert result == ({"msg": "User dont have access"}, HTTPStatus.FORBIDDEN)
-
-    mock_get_jwt_identity.stop()
-    mock_db_get_or_404.stop()
