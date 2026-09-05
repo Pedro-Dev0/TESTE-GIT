4a980413019c0a0c16f0056c7d6423a159ec67ce
Pedro-Dev0 <henripedro652@gmail.com>
2026-09-04 21:35:44 -0300

    testes de integração

--- diobank/tests/integration/controllers/test_user.py
+++ diobank/tests/integration/controllers/test_user.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
 
 def test_get_user_success(client):
-    response = client.get("/users/1")
+    response = client.get("/users/4")
     assert response.status_code == HTTPStatus.OK
