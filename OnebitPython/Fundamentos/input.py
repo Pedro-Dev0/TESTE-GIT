a07ec5e9997c0fb2f7c46e5479d614b58f2306c5
Pedro-Dev0 <henripedro652@gmail.com>
2026-09-09 17:33:09 -0300

    revendo mais partes

---
+++ OnebitPython/Fundamentos/input.py
@@ -0,0 +1,5 @@
+nome = input("Qual o seu nome? \n")
+idade = input("Qual sua idade? \n")
+linguagem = input("Em qual linguagem está programando? \n")
+
+print(f"Olá {nome}, seja bem-vindo ao mundo da programação, sua idade é {idade} e a sua linguagem de aprendizado hoje é {linguagem}!")
---
+++ OnebitPython/Fundamentos/tipo_dados.py
@@ -0,0 +1,12 @@
+idade_texto = "25"
+idade_numero = int(idade_texto)
+nova_idade = idade_numero + 1
+
+print(f"Idade: {nova_idade}")
+
+temperatura = "24.1"
+numero_temperatura = float(temperatura)
+nova_temperatura = numero_temperatura + 1
+
+print(nova_temperatura)
+
